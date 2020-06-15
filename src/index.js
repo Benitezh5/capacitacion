223dcb6e5f7d60768e9078f903b7109b79a05e52
benitezh5 <benitezh5@hotmail.com>
2020-06-15 17:13:22 -0600

    Agregamor el primer componente Main

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Main from './views/main';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 
-    <
-    React.StrictMode >
-    <
-    App / >
-    dd
+    <React.StrictMode >
 
+    <Main/>
+    
 
-    <
-    /React.StrictMode>,
+    </React.StrictMode>,
     document.getElementById('root')
 );
 
---
+++ src/views/main/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Main = () => {
+
+    return (<div>
+        Soy el main
+    </div>
+        )
+
+}
+
+export default Main; 
