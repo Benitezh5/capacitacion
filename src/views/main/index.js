80cab0a3a95ed77bed5cb86abbef0ccc09c1999c
benitezh5 <benitezh5@hotmail.com>
2020-06-15 18:31:38 -0600

    Aprendimos usar los comententes

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,12 +1,31 @@
 import React from "react";
+import HolaMundo from "../holamundo";
+
 
 const Main = () => {
 
-    return (<div>
-        Soy el main
+    return (
+    <div>Soy el main 
+        <HolaMundo name="Humberto" age={28}/>
+        <HolaMundo name="Carlos" age={34} hobby="Programar" />
+    
+    
     </div>
         )
 
 }
+
+/*let obj = {
+    nombre: "Humberto",
+    edad: 28,
+
+  }
+
+  console.table(obj);
+  console.log(`Hola mi nombre es: ${obj.nombre}`);
+  console.log(`Mi edad es: ${obj.edad}`);
+*/
+
+
 
 export default Main; 
